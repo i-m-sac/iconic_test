ac0da719d2253866d7d99b78ae77484af94ac34c
Sac <sachinsreek@gmail.com>
2021-05-27 23:29:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,23 @@ const input = [
   { name: 'links[vive][bundle_id]', value: 'com.vive.htc.package' }
 ]
 
+/**
+ * Function to parse the collection to get the platform and different types of links for it
+ * @param {Array} data 
+ * @returns {Array}
+ */
 const parseDeviceLinks = (data) => {
   const output = []
   const platformLinkMap = {}
   for (const { name, value } of data) {
-    const [platform, linkType] = name.match(/(?<=\[).+?(?=\])/gm)
+    const [platform, linkType] = name.match(/(?<=\[).+?(?=\])/gm) // Regex to capture the string enclosed by square brackets, uses lookahead
     if (platformLinkMap[platform]) {
       platformLinkMap[platform][linkType] = value
     } else {
       platformLinkMap[platform] = {
         [linkType]: value
       }
-      output.push({ [platform]: platformLinkMap[platform] })
+      output.push({ [platform]: platformLinkMap[platform] }) // Setting reference for the object in the array to be reflected for future updates
     }
   }
   return output
